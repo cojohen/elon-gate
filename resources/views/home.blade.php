3e1d7be951fb39b549b9990708e4a42a21c070f2
Joe Cohen <joe@dingocode.com>
2021-09-01 08:13:10 -0500

    Added favicons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Elon Gate: The world's most verbose Elon Musk quote library </title>
+    <title>Elon Gate: The world's premier Elon Musk quote library </title>
     <link 
         rel="stylesheet"
         href="{{ asset('css/app.css') }}"
     />
+    <link 
+        rel="apple-touch-icon" 
+        sizes="180x180" 
+        href="{{ asset('images/apple-touch-icon.png') }}"
+    />
+    <link 
+        rel="icon" 
+        type="image/png" 
+        sizes="32x32" 
+        href="{{ asset('images/favicon-32x32.png') }}"
+    />
+    <link 
+        rel="icon" 
+        type="image/png" 
+        sizes="16x16" 
+        href="{{ asset('images/favicon-16x16.png') }}"
+    />
+    <link 
+        rel="manifest" 
+        href="{{ asset('site.webmanifest') }}"
+    />
+
 </head>
 <body>
     <header>
